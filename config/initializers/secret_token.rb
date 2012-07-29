f9b7bef1e4ff130f804ceff1068bd269b0908f2e
I Sing With Az <isingwithaz@mail.ru>
2012-07-29 23:49:26 +0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Depot2::Application.config.secret_token = '855471c5e5307e711236e727f740358d75107e290931100253d4b561160c93d779c21ed7c8074070f9b73d328813233e497bc1bdf8193f692b8c8961142f7799'
