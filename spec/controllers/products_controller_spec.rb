7c9e0b4cc971c5dbc39bc74da3b5347025f13853
Dzianis Dashkevich <dskecse@gmail.com>
2012-08-01 00:37:26 +0300

    iteration b1. validation

--- spec/controllers/products_controller_spec.rb
+++ spec/controllers/products_controller_spec.rb
@@ -19,49 +19,33 @@ require 'spec_helper'
 # that an instance is receiving a specific message.
 
 describe ProductsController do
-
-  # This should return the minimal set of attributes required to create a valid
-  # Product. As you add validations to Product, be sure to
-  # update the return value of this method accordingly.
-  def valid_attributes
-    {}
-  end
-
-  # This should return the minimal set of values that should be in the session
-  # in order to pass any filters (e.g. authentication) defined in
-  # ProductsController. Be sure to keep this updated too.
-  def valid_session
-    {}
-  end
+  before(:each) { @product = FactoryGirl.create(:product) }
 
   describe "GET index" do
     it "assigns all products as @products" do
-      product = Product.create! valid_attributes
-      get :index, {}, valid_session
-      assigns(:products).should eq([product])
+      get :index
+      assigns(:products).should eq([@product])
     end
   end
 
   describe "GET show" do
     it "assigns the requested product as @product" do
-      product = Product.create! valid_attributes
-      get :show, {:id => product.to_param}, valid_session
-      assigns(:product).should eq(product)
+      get :show, { id: @product.to_param }
+      assigns(:product).should eq(@product)
     end
   end
 
   describe "GET new" do
     it "assigns a new product as @product" do
-      get :new, {}, valid_session
+      get :new
       assigns(:product).should be_a_new(Product)
     end
   end
 
   describe "GET edit" do
     it "assigns the requested product as @product" do
-      product = Product.create! valid_attributes
-      get :edit, {:id => product.to_param}, valid_session
-      assigns(:product).should eq(product)
+      get :edit, { id: @product.to_param }
+      assigns(:product).should eq(@product)
     end
   end
 
@@ -69,18 +53,18 @@ describe ProductsController do
     describe "with valid params" do
       it "creates a new Product" do
         expect {
-          post :create, {:product => valid_attributes}, valid_session
+          post :create, { product: { title: 'MyString2', description: 'MyText', image_url: 'ruby.jpg', price: 9.99 } }
         }.to change(Product, :count).by(1)
       end
 
       it "assigns a newly created product as @product" do
-        post :create, {:product => valid_attributes}, valid_session
+        post :create, { product: { title: 'MyString2', description: 'MyText', image_url: 'ruby.jpg', price: 9.99 } }
         assigns(:product).should be_a(Product)
         assigns(:product).should be_persisted
       end
 
       it "redirects to the created product" do
-        post :create, {:product => valid_attributes}, valid_session
+        post :create, { product: { title: 'MyString2', description: 'MyText', image_url: 'ruby.jpg', price: 9.99 } }
         response.should redirect_to(Product.last)
       end
     end
@@ -89,7 +73,7 @@ describe ProductsController do
       it "assigns a newly created but unsaved product as @product" do
         # Trigger the behavior that occurs when invalid params are submitted
         Product.any_instance.stub(:save).and_return(false)
-        post :create, {:product => {}}, valid_session
+        post :create, { product: {} }
         assigns(:product).should be_a_new(Product)
       end
     end
@@ -98,50 +82,44 @@ describe ProductsController do
   describe "PUT update" do
     describe "with valid params" do
       it "updates the requested product" do
-        product = Product.create! valid_attributes
         # Assuming there are no other products in the database, this
         # specifies that the Product created on the previous line
         # receives the :update_attributes message with whatever params are
         # submitted in the request.
-        Product.any_instance.should_receive(:update_attributes).with({'these' => 'params'})
-        put :update, {:id => product.to_param, :product => {'these' => 'params'}}, valid_session
+        Product.any_instance.should_receive(:update_attributes).with({ 'price' => @product.price.to_s })
+        put :update, { id: @product.to_param, product: { price: @product.price } }
       end
 
       it "assigns the requested product as @product" do
-        product = Product.create! valid_attributes
-        put :update, {:id => product.to_param, :product => valid_attributes}, valid_session
-        assigns(:product).should eq(product)
+        put :update, { id: @product.to_param, product: { price: @product.price } }
+        assigns(:product).should eq(@product)
       end
 
       it "redirects to the product" do
-        product = Product.create! valid_attributes
-        put :update, {:id => product.to_param, :product => valid_attributes}, valid_session
-        response.should redirect_to(product)
+        put :update, { id: @product.to_param, product: { title: 'MyString', description: 'MyText', image_url: 'ruby.jpg', price: 9.99 } }
+        response.should redirect_to(@product)
       end
     end
 
     describe "with invalid params" do
       it "assigns the product as @product" do
-        product = Product.create! valid_attributes
         # Trigger the behavior that occurs when invalid params are submitted
         Product.any_instance.stub(:save).and_return(false)
-        put :update, {:id => product.to_param, :product => {}}, valid_session
-        assigns(:product).should eq(product)
+        put :update, { id: @product.to_param, product: {} }
+        assigns(:product).should eq(@product)
       end
     end
   end
 
   describe "DELETE destroy" do
     it "destroys the requested product" do
-      product = Product.create! valid_attributes
       expect {
-        delete :destroy, {:id => product.to_param}, valid_session
+        delete :destroy, { id: @product.to_param }
       }.to change(Product, :count).by(-1)
     end
 
     it "redirects to the products list" do
-      product = Product.create! valid_attributes
-      delete :destroy, {:id => product.to_param}, valid_session
+      delete :destroy, { id: @product.to_param }
       response.should redirect_to(products_url)
     end
   end
--- spec/factories/products.rb
+++ spec/factories/products.rb
@@ -2,9 +2,9 @@
 
 FactoryGirl.define do
   factory :product do
-    title "MyString"
-    description "MyText"
-    image_url "MyString"
-    price "9.99"
+    title       'MyString'
+    description 'MyText'
+    image_url   'ruby.jpg'
+    price       9.99
   end
 end
