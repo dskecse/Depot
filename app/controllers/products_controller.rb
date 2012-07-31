c8077a3b1c06328a2ad03387c72a43dde26a0a80
Dzianis Dashkevich <dskecse@gmail.com>
2012-07-31 17:47:58 +0300

    Iteration A1: Creating a product maintenance application

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,42 @@
+class ProductsController < ApplicationController
+  def index
+    @products = Product.all
+    respond_with @products
+  end
+
+  def show
+    @product = Product.find(params[:id])
+    respond_with @product
+  end
+
+  def new
+    @product = Product.new
+    respond_with @product
+  end
+
+  def edit
+    @product = Product.find(params[:id])
+    respond_with @product
+  end
+
+  def create
+    @product = Product.new(params[:product])
+    flash[:notice] = 'Product was successfully created.' if @product.save
+    respond_with @product
+  end
+
+  def update
+    @product = Product.find(params[:id])
+    if @product.update_attributes(params[:product])
+      flash[:notice] = 'Product was successfully updated.'
+    end
+    respond_with @product
+  end
+
+  def destroy
+    @product = Product.find(params[:id])
+    @product.destroy
+    flash[:notice] = 'Product was successfully destroyed.' if @product.destroy
+    respond_with @product
+  end
+end
