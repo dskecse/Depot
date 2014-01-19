c0460debc835acca2c2b2669a33ce7af470ed96b
Dzianis Dashkevich <dskecse@gmail.com>
2014-01-19 19:13:17 +0300

    Do not expose session secret

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Depot2::Application.config.secret_token = '69d0d5b549ec27657c1987d78ba3ef41c7cb51cd08343dcdd49a05ed69e1374188ba8817e780f7c459283c7a32ba82c02ea0abfe7594edca4136889bec019a7e'
+Depot2::Application.config.secret_token = AppConfig.secret
