324d8802079472412b06e0a02825b1931bd96733
Dzianis Dashkevich <dskecse@gmail.com>
2014-03-03 22:49:07 +0300

    Generate secure secret key programmaticaly

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,15 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Depot2::Application.config.secret_token = AppConfig.secret
+require 'securerandom'
+
+def secure_token
+  token_file = Rails.root.join('.secret')
+  unless File.exists?(token_file)
+    # Generate a new token and store it in token_file.
+    File.write(token_file, SecureRandom.hex(64))
+  end
+  File.read(token_file).chomp
+end
+
+Depot2::Application.config.secret_token = secure_token
