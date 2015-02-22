1916176efdbf21f83afad4e49470bc5fa055fc19
Dzianis Dashkevich <dskecse@gmail.com>
2015-02-22 14:09:56 +0300

    Update Capfile and config/deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,24 +1,40 @@
-require 'capistrano-deploy'
 require 'rvm/capistrano'
 require 'bundler/capistrano'
 
 set :rvm_ruby_string, '2.0.0@depot'
 
-before 'deploy:setup', 'rvm:install_rvm'
-before 'deploy:setup', 'rvm:install_ruby'
-
-use_recipes :git, :rails, :rails_assets, :bundle, :unicorn
-
-set :application, 'depot_production'
-set :deploy_to, "/home/depot/#{application}"
-
+# set :scm, :git # You can set :scm explicitly or Capistrano will make an intelligent guess based on known version control directory names
+# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
 set :scm, :git
 set :branch, 'master'
 set :repository, 'https://github.com/dskecse/depot.git'
 
+use_recipes :git, :rails, :rails_assets, :bundle, :unicorn
+
+# role :web, "your web-server here"                          # Your HTTP server, Apache/etc
+# role :app, "your app-server here"                          # This may be the same as your `Web` server
+# role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
+# role :db,  "your slave db-server here"
 server '178.62.205.136', :web, :app, :db, primary: true
 
-set :user, 'depot'
+set :user, 'deployer'
+set :application, 'depot_production'
+set :deploy_to, "home/#{user}/#{application}"
+
+# if you want to clean up old releases on each deploy uncomment this:
+# after "deploy:restart", "deploy:cleanup"
+
+# if you're still using the script/reaper helper you will need
+# these http://github.com/rails/irs_process_scripts
+
+# If you are using Passenger mod_rails uncomment this:
+# namespace :deploy do
+#   task :start do ; end
+#   task :stop do ; end
+#   task :restart, :roles => :app, :except => { :no_release => true } do
+#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+#   end
+# end
 
 default_run_options[:pty] = true
 
